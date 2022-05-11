dc301113a75b17c59fce85beb0807ad35ed917a0
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-11 15:31:19 +0900

    Initial Commit

---
+++ Shared/PricingComponentWithToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PricingComponentWithToggleApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct PricingComponentWithToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
