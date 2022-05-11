dd50e61624f0d49ecbdfdd573b6bba47177b90e3
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-11 15:54:06 +0900

    Add the plan models.

---
+++ Shared/Models/Plan.swift
@@ -0,0 +1,43 @@
+//
+//  Plan.swift
+//  Pricing Component WIth Toggle (iOS)
+//
+//  Created by Jinwook Kim on 2022/05/11.
+//
+
+import Foundation
+
+// MARK: PlanType enum.
+
+enum PlanType: String, CaseIterable {
+    case basic = "Basic"
+    case professional = "Professional"
+    case master = "Master"
+}
+
+// MARK: Plan struct.
+
+struct Plan: Identifiable {
+    let id: UUID = .init()
+    let storageCapacity: Int
+    let maxNumOfUsers: Int
+    let sendAllowance: Int
+    let monthlyFee: Double
+    let annuallyFee: Double
+}
+
+extension Plan: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+// MARK: PlanStorage class.
+
+class PlanStorage: ObservableObject {
+    @Published var plans: [PlanType: Plan] = [
+        .basic: .init(storageCapacity: 500, maxNumOfUsers: 2, sendAllowance: 3, monthlyFee: 19.99, annuallyFee: 199.99),
+        .professional: .init(storageCapacity: 1000, maxNumOfUsers: 5, sendAllowance: 10, monthlyFee: 24.99, annuallyFee: 249.99),
+        .master: .init(storageCapacity: 2000, maxNumOfUsers: 10, sendAllowance: 20, monthlyFee: 39.99, annuallyFee: 399.99)
+    ]
+}
